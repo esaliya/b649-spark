fd19d53a7dcd92f99a193e20aa4bf68a67952673
Saliya Ekanayake <esaliya@gmail.com>
2016-04-21 13:13:06 -0400

    removing primer code

--- src/main/java/edu/indiana/cs/b649/spark/SparkPrimer.java
+++ src/main/java/edu/indiana/cs/b649/spark/SparkPrimer.java
@@ -9,12 +9,6 @@ import java.util.List;
 
 public class SparkPrimer {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Primer");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> distData = sc.parallelize(data);
-        int x = distData.reduce((i,j) -> i+j);
-        System.out.println("\n**Reduced Output = " + x + "\n");
+        // Your's to fill
     }
 }
