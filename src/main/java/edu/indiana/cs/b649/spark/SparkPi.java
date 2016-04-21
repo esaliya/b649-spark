310a146eb3d7f8ad497bd602d38c0d56b7d6b4a3
Saliya Ekanayake <esaliya@gmail.com>
2016-04-21 03:35:13 -0400

    Adding pi

--- src/main/java/edu/indiana/cs/b649/spark/SparkPi.java
+++ src/main/java/edu/indiana/cs/b649/spark/SparkPi.java
@@ -11,13 +11,20 @@ public class SparkPi {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Pi");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // The number of samples we like to check
         int NUM_SAMPLES = 100;
 
-        List<Integer> l = new ArrayList<Integer>(NUM_SAMPLES);
+        // Create a list of numbers from 0 to NUM_SAMPLES - 1
+        List<Integer> l = new ArrayList<>(NUM_SAMPLES);
         for (int i = 0; i < NUM_SAMPLES; i++) {
             l.add(i);
         }
 
+        // Make an RDD from the numbers and execute filter transformation in
+        // parallel followed by a count action.
+        // Note. we actually ignore the number value (i). Usually, this
+        // pattern is called iterating over an index set in parallel.
         long count = sc.parallelize(l).filter((Function<Integer, Boolean>) i -> {
             double x = Math.random();
             double y = Math.random();
