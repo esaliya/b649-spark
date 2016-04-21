a2ee7d25583e85f0daadcb069c2391fde56ff1b0
Saliya Ekanayake <esaliya@gmail.com>
2016-04-21 01:28:14 -0400

    Adding spark examples

---
+++ src/main/java/edu/indiana/cs/b649/spark/SparkPrimer.java
@@ -0,0 +1,20 @@
+package edu.indiana.cs.b649.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SparkPrimer {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("HelloWorld");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> distData = sc.parallelize(data);
+        int x = distData.reduce((i,j) -> i+j);
+        System.out.println(x);
+    }
+}
