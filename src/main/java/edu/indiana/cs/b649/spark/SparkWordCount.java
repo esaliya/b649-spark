62cb900b63590907d840d53cbdcbba05bcd7585c
Saliya Ekanayake <esaliya@gmail.com>
2016-04-21 13:31:46 -0400

    removing master local

--- src/main/java/edu/indiana/cs/b649/spark/SparkWordCount.java
+++ src/main/java/edu/indiana/cs/b649/spark/SparkWordCount.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 
 public class SparkWordCount {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName
+        SparkConf conf = new SparkConf().setAppName
                 ("WordCount");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
