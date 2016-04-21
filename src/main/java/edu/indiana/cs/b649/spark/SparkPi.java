ab289c4698cd0db9a64f8ed4c678d3f6f721db5b
Saliya Ekanayake <esaliya@gmail.com>
2016-04-21 03:31:41 -0400

    Adding pi

--- src/main/java/edu/indiana/cs/b649/spark/SparkPi.java
+++ src/main/java/edu/indiana/cs/b649/spark/SparkPi.java
@@ -1,7 +1,28 @@
 package edu.indiana.cs.b649.spark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SparkPi {
     public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("Pi");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        int NUM_SAMPLES = 100;
 
+        List<Integer> l = new ArrayList<Integer>(NUM_SAMPLES);
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            l.add(i);
+        }
+
+        long count = sc.parallelize(l).filter((Function<Integer, Boolean>) i -> {
+            double x = Math.random();
+            double y = Math.random();
+            return x*x + y*y < 1;
+        }).count();
+        System.out.println("Pi is roughly " + 4.0 * count / NUM_SAMPLES);
     }
 }
